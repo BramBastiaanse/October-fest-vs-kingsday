4bb7710ac9163710348c0aa8333d51463000c3b3
Robbin de Waal <robbindewaal@robbins-mbp.wireless.hhs.nl>
2021-09-21 15:07:27 +0200

    Saved data

--- src/data-prep/Untitled.R
+++ src/data-prep/Untitled.R
@@ -1,4 +1,4 @@
 #Getting the 
 
-read.csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-08-06/visualisations/listings.csv")
-View(read.csv)
+listings_amsterdam <- read.csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-08-06/visualisations/listings.csv")
+View(listings_amsterdam)
