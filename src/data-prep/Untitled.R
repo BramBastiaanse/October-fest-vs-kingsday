240931f589c49bbcc5741b72467f3e2e9d6918eb
Robbin de Waal <robbindewaal@Robbins-MacBook-Pro.local>
2021-09-22 11:36:05 +0200

    Removed and renamed files

--- src/data-prep/Untitled.R
+++
@@ -1,4 +0,0 @@
-#Getting the 
-
-listings_amsterdam <- read.csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-08-06/visualisations/listings.csv")
-View(listings_amsterdam)
