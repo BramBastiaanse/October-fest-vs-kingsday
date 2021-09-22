1c3be1d45e6a274ba09c29989c77924397987d2c
Robbin de Waal <robbindewaal@Robbins-MacBook-Pro.local>
2021-09-22 11:51:15 +0200

    included singapore_calendar and shanghai_calendar

--- data/Data codes.R
+++ data/Data codes.R
@@ -1,10 +1,24 @@
 # Getting data for Shanghai and Singapore
-#Shanghai
-read.csv("http://data.insideairbnb.com/china/shanghai/shanghai/2021-07-31/visualisations/listings.csv")
-shanghai <- read.csv("http://data.insideairbnb.com/china/shanghai/shanghai/2021-07-31/visualisations/listings.csv")
-View(shanghai)
+## Shanghai
+# Listings
+shanghai_listing <- read.csv("http://data.insideairbnb.com/china/shanghai/shanghai/2021-07-31/visualisations/listings.csv")
+View(shanghai_listing)
 
-#Singapore
+# Calendar
+library(data.table)
+install.packages("R.utils")
+library(r.utils)
+shanghai_calendar <- fread("http://data.insideairbnb.com/china/shanghai/shanghai/2021-07-31/data/calendar.csv.gz")
+View(shanghai_calendar)
+
+
+
+# Singapore 
+# Listing
 read.csv("http://data.insideairbnb.com/singapore/sg/singapore/2021-07-19/visualisations/listings.csv")
-singapore <-read.csv("http://data.insideairbnb.com/singapore/sg/singapore/2021-07-19/visualisations/listings.csv")
-View(singapore)
+singapore_listing <-read.csv("http://data.insideairbnb.com/singapore/sg/singapore/2021-07-19/visualisations/listings.csv")
+View(singapore_listing)
+
+# calendar
+Singapore_calendar <- fread("http://data.insideairbnb.com/singapore/sg/singapore/2021-07-19/data/calendar.csv.gz")
+View(Singapore_calendar)
