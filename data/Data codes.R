45a4890e79c409c3667fdc5a813da2aeab207618
NielsRahder <n.j.rahder@tilburguniversity.edu>
2021-09-24 16:08:58 +0200

    Data prep on shanghai

--- data/Data codes.R
+++ data/Data codes.R
@@ -23,3 +23,18 @@ View(singapore_listing)
 Singapore_calendar <- fread("http://data.insideairbnb.com/singapore/sg/singapore/2021-07-19/data/calendar.csv.gz")
 View(Singapore_calendar)
 
+View(singapore_calendar)
+
+#create subset & exclude variables
+Shanghai_total <- subset(shanghai_listing, select = -c (host_name, last_review, reviews_per_month, calculated_host_listings_count, license, neighbourhood_group, minimum_nights, number_of_reviews_ltm))
+View(Shanghai_total)
+
+#rename listing_id to id
+colnames(shanghai_calendar)[colnames(shanghai_calendar) == "listing_id"] <- 'id'
+
+#Merge shanghai_calendar with shanghai_total based on 'id'
+shanghain_total <- merge(Shanghai_total, shanghai_calendar, by ='id')
+
+#delete rows from shaghai_total that dont match with shaghai_calendar by 'id'
+unwanted <-which(!rownames(Shanghai_total) %in% rownames(shaghai_calendar))    
+environment2<-shanghain_total[-unwanted,]
