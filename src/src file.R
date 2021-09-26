30059f884bca2363d6439d182365cafc914b3b41
robbindewaal <rs.dewaal@tilburguniversity.edu>
2021-09-26 13:34:59 +0200

    put the commands in the right folder (from data to src)

--- src/src file.R
+++ src/src file.R
@@ -1,1 +1,42 @@
 #src file
+# Install required packages
+library(data.table)
+install.packages("R.utils")
+library(R.utils)
+library(dplyr)
+
+
+#create subset & exclude variables
+Shanghai_total <- subset(shanghai_listing, select = -c (host_name, last_review, reviews_per_month, calculated_host_listings_count, license, neighbourhood_group, minimum_nights, number_of_reviews_ltm))
+
+singapore_total <- subset(singapore_listing, select = -c (host_name, last_review, reviews_per_month, calculated_host_listings_count, license, neighbourhood_group, minimum_nights, number_of_reviews_ltm))
+
+#rename listing_id to id
+colnames(shanghai_calendar)[colnames(shanghai_calendar) == "listing_id"] <- 'id'
+
+colnames(Singapore_calendar)[colnames(Singapore_calendar) == "listing_id"] <- 'id'
+
+#Merge _calendar with _total based on 'id'
+shanghai_total1 <- merge(Shanghai_total, shanghai_calendar, by ='id')
+singapore_total1 <- merge(singapore_total, Singapore_calendar, by ='id')
+
+View(shanghai_total1)
+View(singapore_total1)
+
+# Create new datasets filtered on event date
+singapore_grandprix <- singapore_total1 %>%
+  filter(date >= "2022-04-22" & date <= "2022-04-24")
+
+shanghai_grandprix <- shanghai_total1 %>%
+  filter(date >= "2022-04-22" & date <= "2022-04-24")
+
+View(shanghai_grandprix)
+
+
+#delete rows from shaghai_total that dont match with shaghai_calendar by 'id'
+unwanted <-which(!rownames(Shanghai_total) %in% rownames(shanghai_calendar))    
+environment2<-Shanghai_total[-unwanted,]
+
+unwanted <-which(!rownames(Shanghai_total) %in% rownames(shanghai_calendar))    
+environment2<-Shanghai_total[-unwanted,]
+
