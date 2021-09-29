24b748b181ed42ec066a37a8b2fff4cc1514ef37
robbindewaal <rs.dewaal@tilburguniversity.edu>
2021-09-29 20:19:19 +0200

    Tried to get the dummy variable to work

--- src/src file.R
+++ src/src file.R
@@ -35,6 +35,17 @@ colnames(Singapore_calendar)[colnames(Singapore_calendar) == "listing_id"] <- 'i
 shanghai_total1 <- merge(Shanghai_total, shanghai_calendar, by ='id')
 singapore_total1 <- merge(singapore_total, Singapore_calendar, by ='id')
 
+# Create dummy variabe
+shanghai_total1%race_weekend <-  shanghai_total1$date >=  "2022-04-22" & shanghai_total1$date <= "2022-04-24" 
+
+#delete rows from shaghai_total that dont match with shaghai_calendar by 'id'
+unwanted <-which(!rownames(Shanghai_total) %in% rownames(shanghai_calendar))    
+environment2<-Shanghai_total[-unwanted,]
+
+unwanted <-which(!rownames(Shanghai_total) %in% rownames(shanghai_calendar))    
+environment2<-Shanghai_total[-unwanted,]
+
+
 # Create new datasets filtered on event date
 singapore_grandprix <- singapore_total1 %>%
   filter(date >= "2022-04-22" & date <= "2022-04-24")
@@ -44,12 +55,4 @@ shanghai_grandprix <- shanghai_total1 %>%
 
 View(shanghai_grandprix)
 View(singapore_grandprix)
-
-
-#delete rows from shaghai_total that dont match with shaghai_calendar by 'id'
-unwanted <-which(!rownames(Shanghai_total) %in% rownames(shanghai_calendar))    
-environment2<-Shanghai_total[-unwanted,]
-
-unwanted <-which(!rownames(Shanghai_total) %in% rownames(shanghai_calendar))    
-environment2<-Shanghai_total[-unwanted,]
 
