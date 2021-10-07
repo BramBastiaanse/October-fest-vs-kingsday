40f030d7eda1190edf0b851cea3b6eb01f38e0e7
robbindewaal <rs.dewaal@tilburguniversity.edu>
2021-10-07 15:20:52 +0200

    re-introduced the download data

--- src/datapreperation.R
+++ src/datapreperation.R
@@ -7,6 +7,13 @@ library(readr)
 library(car)
 library(ggplot2)
 
+#Download data
+shanghai_listing <- read.csv("http://data.insideairbnb.com/china/shanghai/shanghai/2021-07-31/visualisations/listings.csv")
+shanghai_calendar <- fread("http://data.insideairbnb.com/china/shanghai/shanghai/2021-07-31/data/calendar.csv.gz")
+
+spain_listing <- read.csv("http://data.insideairbnb.com/spain/catalonia/barcelona/2021-07-07/visualisations/listings.csv")
+spain_calendar <- fread("http://data.insideairbnb.com/spain/catalonia/barcelona/2021-07-07/data/calendar.csv.gz")
+
 
 ## TRANSFORMATION ##
 
