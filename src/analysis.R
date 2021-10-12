12d1f102748b53341c01d3b39a1b9923d5964dcf
BramBastiaanse <b.bastiaanse@tilburguniversity.edu>
2021-10-12 15:40:08 +0200

    Created sink file

--- src/analysis.R
+++ src/analysis.R
@@ -11,6 +11,10 @@ library(stargazer)
 shanghai_total1_lm1 <- lm(shanghai_total1$adjprice_corrected  ~ race_weekend + distance + type_of_room + distance, shanghai_total1)
 summary(shanghai_total1_lm1)
 
+sink(file = "lm_output")
+summary(shanghai_total1_lm1)
+sink(file = NULL)
+
 shanghai_total1_lm2 <-  lm(shanghai_total1$adjprice_corrected ~ race_weekend, shanghai_total1 )
 
 
@@ -19,7 +23,16 @@ plot(shanghai_total1_lm2)
 samplesize=1E3
 shanghai_total1_lm2 <-  lm(shanghai_total1$adjprice_corrected ~ race_weekend, shanghai_total1, subset = sample(1:nrow(shangai_total1, samplesize)))
 
-
+stargazer(shanghai_total1_lm1, 
+          title = "title", 
+          dep.var.caption = "caption",
+          dep.var.labels = "",
+          covariate.labels = c("race_weekend", "distance", "type_of_room"),
+          column.labels = c("estimate", "std. error", "t-value", "Pr(>|t|)"),
+          notes.label = "signifance levels",
+          type = "html",
+          out = "output.html")
+          
 
 
 
