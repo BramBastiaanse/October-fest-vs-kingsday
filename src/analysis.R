cb77bbb453d121ed9660be10dd9040ec51067966
robbindewaal <rs.dewaal@tilburguniversity.edu>
2021-10-12 14:45:08 +0200

    created spain regression

--- src/analysis.R
+++ src/analysis.R
@@ -11,8 +11,8 @@ library(stargazer)
 shanghai_total1_lm1 <- lm(shanghai_total1$adjprice_corrected  ~ race_weekend + distance + type_of_room + distance, shanghai_total1)
 summary(shanghai_total1_lm1)
 
-shanghai_total1_lm2 <-  lm(shanghai_total1$adjprice_corrected ~ race_weekend, shanghai_total1 )
-
+spain_total1_lm1 <-  lm(spain_total1$adjprice_corrected ~ race_weekend + distance + type_of_room + distance, spain_total1)
+summary(spain_total1_lm1)
 
 plot(shanghai_total1_lm2)
 
