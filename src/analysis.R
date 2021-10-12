0fadaafa64c8b783838fb851901bb32544d9c961
robbindewaal <rs.dewaal@tilburguniversity.edu>
2021-10-12 14:24:41 +0200

    Changed gitignore and anlysis

--- src/analysis.R
+++ src/analysis.R
@@ -1,23 +1,27 @@
+library(ggplot2)
+library(ggplot2)
+library(ggfortify)
+library(broom)
+library(dplyr)
+library(stargazer)
+
 # Regression Models
+
 
 shanghai_total1_lm1 <- lm(shanghai_total1$adjprice_corrected  ~ race_weekend + distance + type_of_room + distance, shanghai_total1)
 summary(shanghai_total1_lm1)
 
-shanghai_total1_lm2 <- lm(shanghai_total1$adjprice_corrected ~ shanghai_total1)
-summary(shanghai_total1_lm1)
-summary(shanghai_total1_lm2)
+shanghai_total1_lm2 <-  lm(shanghai_total1$adjprice_corrected ~ race_weekend, shanghai_total1 )
 
-spain_total1_lm1 <- lm(spain_total1$adjprice_corrected ~ race_weekend + distance, spain_total1)
-spain_total1_lm2 <- lm(spain_total1$adjprice_corrected ~ type_of_room + distance, spain_total1)
-summary(spain_total1_lm1)
-summary(spain_total1_lm2)
 
-# Plots
-ggplot(shanghai_total1, aes(x=adjprice_corrected, y=race_weekend)) +
-  geom_point()
-ggplot(sat, aes(x=adjprice_corrected, y=type_of_room)) +
-  geom_point()
-ggplot(sat, aes(x=adjprice_corrected, y=distance)) +
-  geom_point()
+plot(shanghai_total1_lm2)
 
-pdf("plot all pdf")
+samplesize=1E3
+shanghai_total1_lm2 <-  lm(shanghai_total1$adjprice_corrected ~ race_weekend, shanghai_total1, subset = sample(1:nrow(shangai_total1, samplesize)))
+
+
+
+
+
+
+
